8c8fe4edded248853f8e039cf5673b9329fe2d96
amirulaiman26 <94789480+amirulaiman26@users.noreply.github.com>
2021-12-02 00:17:34 +0800

    Change UI for booking workshop

--- lib/Screens/book_workshop/book_detail.dart
+++ lib/Screens/book_workshop/book_detail.dart
@@ -4,7 +4,14 @@ import 'package:flutter_auth/components/rounded_button.dart';
 import 'package:flutter_auth/constants.dart';
 
 class BookingDetail extends StatelessWidget{
-  const BookingDetail({key}) : super(key: key);
+  List<String> items = <String>[
+    'Select', 'Session 1: 24/11/2021 10:00 AM - 12:00 PM', 
+    'Session 2: 2/12/2021 2:00 PM - 5:00 PM',
+    'Session 3: 14/1/2022 2:00 PM - 5:00 PM',
+    'Session 4: 30/1/2022 10:00 AM - 1:00 PM',
+    
+  ];
+  String dropdownValue = 'Select';
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
@@ -53,86 +60,45 @@ class BookingDetail extends StatelessWidget{
                   SizedBox(height: size.height*0.02,),
                 ],)
               ),
-              Container(
-                padding: EdgeInsets.symmetric(
-                  horizontal: size.width*0.05,
-                  vertical: size.height*0.03,
-                ),
-                margin: const EdgeInsets.only(top: 10),
-                width: double.infinity,
-                decoration: BoxDecoration(color: Colors.white,
-                borderRadius: BorderRadius.circular(15)),
-                child: Row(children: [
-                  Text('Session 1:', style: TextStyle(fontSize: size.width*0.034, fontWeight: FontWeight.bold),),
-                  SizedBox(height: size.height*0.02,),
-                  Row(children: [
-                    Text(' 24/11/2021', style: TextStyle(fontSize: size.width*0.034,),),
-                  ],),
-                  SizedBox(height: size.height*0.034,),
-                    Text(' 10:00 AM - 12:00 PM',)
-                ],),
-                ),
-
+              SizedBox(height: 27,),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Select session', style: TextStyle(fontWeight: FontWeight.bold),)
+                ],
+              ),
                 Container(
-                padding: EdgeInsets.symmetric(
-                  horizontal: size.width*0.05,
-                  vertical: size.height*0.03,
+                  padding: EdgeInsets.symmetric(
+                  horizontal: 12,
+                  vertical: 4,
                 ),
                 margin: const EdgeInsets.only(top: 10),
                 width: double.infinity,
                 decoration: BoxDecoration(color: Colors.white,
                 borderRadius: BorderRadius.circular(15)),
-                child: Row(children: [
-                  Text('Session 2:', style: TextStyle(fontSize: size.width*0.034, fontWeight: FontWeight.bold),),
-                  SizedBox(height: size.height*0.02,),
-                  Row(children: [
-                    Text(' 2/12/2021', style: TextStyle(fontSize: size.width*0.034,),),
-                  ],),
-                  SizedBox(height: size.height*0.034,),
-                    Text(' 2:00 PM - 5:00 PM',)
-                ],),
+                  child: DropdownButtonHideUnderline(
+                  child: DropdownButton<String>(
+                    onChanged: (String? newValue){
+                      setState(() {
+                        dropdownValue = newValue!;
+                      }
+                      );
+                    },
+                    value: dropdownValue,
+                    items: items.map<DropdownMenuItem<String>>(
+                      (String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                          );
+                      },
+                    ).toList(),
+                    ),
+                  ),
                 ),
-
-                Container(
-                padding: EdgeInsets.symmetric(
-                  horizontal: size.width*0.05,
-                  vertical: size.height*0.03,
-                ),
-                margin: const EdgeInsets.only(top: 10),
-                width: double.infinity,
-                decoration: BoxDecoration(color: Colors.white,
-                borderRadius: BorderRadius.circular(15)),
-                child: Row(children: [
-                  Text('Session 3:', style: TextStyle(fontSize: size.width*0.034, fontWeight: FontWeight.bold),),
-                  SizedBox(height: size.height*0.02,),
-                  Row(children: [
-                    Text(' 14/1/2022', style: TextStyle(fontSize: size.width*0.034,),),
-                  ],),
-                  SizedBox(height: size.height*0.034,),
-                    Text(' 2:00 PM - 5:00 PM',)
-                ],),
-                ),
-                Container(
-                padding: EdgeInsets.symmetric(
-                  horizontal: size.width*0.05,
-                  vertical: size.height*0.03,
-                ),
-                margin: const EdgeInsets.only(top: 10),
-                width: double.infinity,
-                decoration: BoxDecoration(color: Colors.white,
-                borderRadius: BorderRadius.circular(15)),
-                child: Row(children: [
-                  Text('Session 4:', style: TextStyle(fontSize: size.width*0.034, fontWeight: FontWeight.bold),),
-                  SizedBox(height: size.height*0.02,),
-                  Row(children: [
-                    Text(' 30/1/2022', style: TextStyle(fontSize: size.width*0.034,),),
-                  ],),
-                  SizedBox(height: size.height*0.034,),
-                    Text(' 10:00 AM - 1:00 PM',)
-                ],),
-                ),
-                SizedBox(height: 160,),
+                SizedBox(height: 380,),
                 RoundedButton(
+                
                 text: "Apply",
                 
                 press: () {
@@ -144,5 +110,20 @@ class BookingDetail extends StatelessWidget{
 
     
   }
+  void showAlertDialog(BuildContext context) => showDialog(
+    builder: (context) => AlertDialog(
+      title: Text('Testing'),
+      content: Text('Trying'),
+      actions: [
+        OutlineButton(
+          onPressed:() => Navigator.of(context).pop(),
+          child: Text('Confirm'),
+           )
+      ],
+    ), context: context
+    );
+
+
+  void setState(Null Function() param0) {}
 
 }
